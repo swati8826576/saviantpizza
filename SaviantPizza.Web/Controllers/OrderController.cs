6ec20f5afd723c8876ce749d5e3b66f2da47d6ad
swati <swati.goswami2010@gmail.com>
2020-06-30 21:01:30 +0530

    test

--- SaviantPizza.Web/Controllers/OrderController.cs
+++ SaviantPizza.Web/Controllers/OrderController.cs
@@ -9,6 +9,7 @@ namespace SaviantPizza.Web.Controllers
     public class OrderController : ControllerBase
     {
         [HttpGet]
+
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
